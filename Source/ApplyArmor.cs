5716b5d6f9d5e985fccfe4d37da6c30422e36ec2
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-02-17 16:33:24 -0800

    Armor penetration reduced, including future apparel Reduced by P*=2P/R so if penetration rips through armor, it is not reduced
    using delegate method instead of nasty MethodInfo
    
    Logging last shot to see in settings window

--- Source/ApplyArmor.cs
+++ Source/ApplyArmor.cs
@@ -12,35 +12,55 @@ using HarmonyLib;
 
 namespace ThickArmor
 {
+	public delegate void ApplyArmorDelegate(ref float damAmount, float armorPenetration, float armorRating, Thing armorThing, ref DamageDef damageDef, Pawn pawn, out bool metalArmor);
+
 	[HarmonyPatch(typeof(ArmorUtility))]
 	[HarmonyPatch("GetPostArmorDamage")]
 	//public static float GetPostArmorDamage(Pawn pawn, float amount, float armorPenetration, BodyPartRecord part, ref DamageDef damageDef, out bool deflectedByMetalArmor, out bool diminishedByMetalArmor)
-	class GetPostArmorDamage_Patch
+	public static class GetPostArmorDamage_Patch
 	{
-		static MethodInfo ApplyArmorInfo = AccessTools.Method(typeof(ArmorUtility), "ApplyArmor");
-		//private static void ApplyArmor(ref float damAmount, float armorPenetration, float armorRating, Thing armorThing, ref DamageDef damageDef, Pawn pawn, out bool metalArmor)
-		public static void ApplyArmorLayered(ref float damAmount, float armorPenetration, float armorRating, Thing armorThing, ref DamageDef damageDef, Pawn pawn, out bool metalArmor)
+		public static string lastShotlog = "";
+		public static void Log(string s)
 		{
-			Log.Message($"Doing it, damAmount= {damAmount}, armorPenetration = {armorPenetration}, armorRating = {armorRating}, armorThing = {armorThing}" +
-				$", damageDef = {damageDef}, pawn = {pawn}");
-			int layers = armorThing?.def.apparel.layers.Count ?? 1;
+			Verse.Log.Message(s);
+			lastShotlog += s + "\n";
+		}
 
-			var args = new object[] { damAmount, armorPenetration, armorRating, armorThing, damageDef, pawn, false };
+		public static void Prefix()
+		{
+			lastShotlog = "";
+		}
+
+
+		public static ApplyArmorDelegate ApplyArmorInfo = AccessTools.MethodDelegate<ApplyArmorDelegate>(AccessTools.Method(typeof(ArmorUtility), "ApplyArmor"));
+
+		//private static void ApplyArmor(ref float damAmount, float armorPenetration, float armorRating, Thing armorThing, ref DamageDef damageDef, Pawn pawn, out bool metalArmor)
+		public static void ApplyArmorLayered(ref float damAmount, ref float armorPenetration, float armorRating, Thing armorThing, ref DamageDef damageDef, Pawn pawn, out bool metalArmor)
+		{
+			int layers = armorThing?.def.apparel.layers.Count ?? 1;
+			Log($"Doing ThickArmor for {pawn} wearing {armorThing?.LabelCap ?? "nothing"}");
+			Log($"{layers} layers		Damage		Penetration	Armor		Type		Metal");
+			Log($"START		{damAmount:0.0}		{armorPenetration:0.0%}		{armorRating :0.0%}		{damageDef}		False");
 
 			do
 			{
-				//Call it
-				ApplyArmorInfo.Invoke(null, args);
-				metalArmor = (bool)args[6];
-				damageDef = (DamageDef)args[4];
-				damAmount = (float)args[0];
-				Log.Message($"layer {layers}, damAmount= {damAmount}, damageDef = {damageDef}, pawn = {pawn}, metalArmor = {metalArmor}");
+				//float previousAmount = damAmount;
 
-				//Dampen effects
+				//Call it
+				ApplyArmorInfo.Invoke(ref damAmount, armorPenetration, armorRating, armorThing, ref damageDef, pawn, out metalArmor);
+
+				//if (previousAmount == damAmount) //only if damage not reduced
+				if (Mod.settings.penetrationReduction &&
+					armorRating > 2*armorPenetration)
+					armorPenetration *= 2*armorPenetration / armorRating;
+				//If armor rating is <= 2xpenetration, no penetration reduction
+				//If armor rating is e.g. 4x penetration, reduce penetration 50%
+				//If armor rating >> penetration, reduce penetration to near-0
+
+				//Dampen armor rating
 				armorRating *= Mod.settings.secondLayerEffectiveness;
 
-				//Apply new value
-				args[2] = armorRating;
+				Log($"After {layers}:		{damAmount:0.0}		{armorPenetration:0.0%}		{armorRating:0.0%}		{damageDef}		{metalArmor}");
 			}
 			while (--layers > 0 && damAmount > 0);
 		}
@@ -53,8 +73,9 @@ namespace ThickArmor
 
 			foreach (CodeInstruction instruction in instructions)
 			{
-				//IL_0068: call         void Verse.ArmorUtility::ApplyArmor(float32 &, float32, class Verse.Thing, class Verse.DamageDef)
-				if (instruction.Calls(ApplyArmorInfo))
+				if (instruction.opcode == OpCodes.Ldarg_2)  //change armorPenetration to ref armorPenetration (big problem if later patches use armorPenentration elswhere)
+					yield return new CodeInstruction(OpCodes.Ldarga_S, 2);
+				else if (instruction.Calls(ApplyArmorInfo))
 					yield return new CodeInstruction(OpCodes.Call, ApplyArmorLayeredInfo);
 				else 
 					yield return instruction;
--- Source/Settings.cs
+++ Source/Settings.cs
@@ -7,7 +7,8 @@ namespace ThickArmor
 {
 	public class Settings : ModSettings
 	{
-		public float secondLayerEffectiveness = 0.6f;
+		public float secondLayerEffectiveness = 0.5f;
+		public bool penetrationReduction = true;
 
 		public void DoWindowContents(Rect wrect)
 		{
@@ -15,15 +16,32 @@ namespace ThickArmor
 			options.Begin(wrect);
 
 			options.Label("TD.ArmorExplanation".Translate());
+			Text.Font = GameFont.Medium;
 			options.Label("TD.NextLayerEffectiveness".Translate() + String.Format("{0:0}%", secondLayerEffectiveness * 100));
+			Text.Font = GameFont.Small;
 			secondLayerEffectiveness = options.Slider(secondLayerEffectiveness, 0.0f, 1.0f);
+			options.Label("TD.PenetrationExplanation".Translate());
+			Text.Font = GameFont.Medium;
+			options.CheckboxLabeled("TD.PenetrationReduction".Translate(), ref penetrationReduction);
+			Text.Font = GameFont.Small;
+
+			if (GetPostArmorDamage_Patch.lastShotlog != "")
+			{
+				options.Gap(50f);
+				options.GapLine();
+				Text.Font = GameFont.Medium;
+				options.Label("Effects of Thick Armor on Last Shot:");
+				Text.Font = GameFont.Small;
+				options.Label(GetPostArmorDamage_Patch.lastShotlog);
+			}
 
 			options.End();
 		}
 		
 		public override void ExposeData()
 		{
-			Scribe_Values.Look(ref secondLayerEffectiveness, "secondLayerEffectiveness", 0.6f);
+			Scribe_Values.Look(ref secondLayerEffectiveness, "secondLayerEffectiveness", 0.5f);
+			Scribe_Values.Look(ref penetrationReduction, "penetrationReduction", true);
 		}
 	}
 }
